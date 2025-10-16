f1b8db4e94ee2a1630c1636d11f0cacb0a711168
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-10-16 13:47:41 +0500

    evaluation done

---
+++ evaluate_model.py
@@ -0,0 +1,6 @@
+from sklearn.metrics import accuracy_score, classification_report
+
+def evaluate(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Classification Report:\n", classification_report(y_test, y_pred))
