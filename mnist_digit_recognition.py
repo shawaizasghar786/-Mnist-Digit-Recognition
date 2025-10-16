86aeb7a9bda48b17104650eae9ec8087bfddbb5b
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-10-16 13:57:51 +0500

    path updated

--- mnist_digit_recognition.py
+++ mnist_digit_recognition.py
@@ -1,13 +1,15 @@
-from src.load_data import load_images, load_labels
-from src.preprocess import preprocess_data
-from src.train_model import train_classifier
-from src.evaluate_model import evaluate  
+from load_data import load_images, load_labels
+from process import preprocess_data
+from train_model import train_classifier
+from evaluate_model import evaluate
+
 
 # Load data
-train_images = load_images("MNIST/train-images-idx3-ubyte")
-train_labels = load_labels("MNIST/train-labels-idx1-ubyte")
-test_images = load_images("MNIST/t10k-images-idx3-ubyte")
-test_labels = load_labels("MNIST/t10k-labels-idx1-ubyte")
+train_images = load_images("MNIST/train-images.idx3-ubyte")
+train_labels = load_labels("MNIST/train-labels.idx1-ubyte")
+test_images = load_images("MNIST/t10k-images.idx3-ubyte")
+test_labels = load_labels("MNIST/t10k-labels.idx1-ubyte")
+
 
 # Preprocess
 X_train, X_test, y_train, y_test = preprocess_data(train_images, test_images, train_labels, test_labels)
